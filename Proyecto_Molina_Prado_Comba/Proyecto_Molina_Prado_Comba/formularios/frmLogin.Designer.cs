73585a197911259b2f93af664821f4aec37342b5
lycomba <lycomba@users.noreply.github.com>
2019-09-03 12:22:51 -0300

    Create frmLogin.Designer.cs

---
+++ Proyecto_Molina_Prado_Comba/Proyecto_Molina_Prado_Comba/formularios/frmLogin.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Proyecto_Molina_Prado_Comba
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brnAceptar = new System.Windows.Forms.Button();
+            this.brnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // brnAceptar
+            // 
+            this.brnAceptar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brnAceptar.Location = new System.Drawing.Point(46, 157);
+            this.brnAceptar.Name = "brnAceptar";
+            this.brnAceptar.Size = new System.Drawing.Size(87, 32);
+            this.brnAceptar.TabIndex = 3;
+            this.brnAceptar.Text = "Aceptar";
+            this.brnAceptar.UseVisualStyleBackColor = true;
+            this.brnAceptar.Click += new System.EventHandler(this.brnAceptar_Click);
+            // 
+            // brnSalir
+            // 
+            this.brnSalir.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brnSalir.Location = new System.Drawing.Point(207, 157);
+            this.brnSalir.Name = "brnSalir";
+            this.brnSalir.Size = new System.Drawing.Size(87, 32);
+            this.brnSalir.TabIndex = 4;
+            this.brnSalir.Text = "Salir";
+            this.brnSalir.UseVisualStyleBackColor = true;
+            this.brnSalir.Click += new System.EventHandler(this.brnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 23);
+            this.label1.TabIndex = 99;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 23);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(160, 46);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(134, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(160, 97);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(134, 20);
+            this.txtContraseña.TabIndex = 2;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 213);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.brnSalir);
+            this.Controls.Add(this.brnAceptar);
+            this.Name = "frmLogin";
+            this.Text = "t";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button brnAceptar;
+        private System.Windows.Forms.Button brnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+    }
+}
