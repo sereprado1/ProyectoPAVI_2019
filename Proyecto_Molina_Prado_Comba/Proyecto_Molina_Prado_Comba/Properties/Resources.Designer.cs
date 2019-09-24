5b61443a9f2561e71564f6bde3899bccc927b22e
sereprado1 <serepocket1@gmail.com>
2019-09-23 21:59:24 -0300

    Imagenes

--- Proyecto_Molina_Prado_Comba/Proyecto_Molina_Prado_Comba/Properties/Resources.Designer.cs
+++ Proyecto_Molina_Prado_Comba/Proyecto_Molina_Prado_Comba/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace Proyecto_Molina_Prado_Comba.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Fondo1 {
+            get {
+                object obj = ResourceManager.GetObject("Fondo1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
